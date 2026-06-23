7d472192dd0dc6156b6ea58611188e8f512dc549
NaufalKhalil <naufalkhalil.third@gmail.com>
2026-06-24 05:16:08 +0700

    update: 2026-06-24 05:16

--- Python/L090-Menampilkan Angka Terdekat dengan Rata-rata.py
+++ Python/L090-Menampilkan Angka Terdekat dengan Rata-rata.py
@@ -2,25 +2,25 @@ angka = []
 total = 0
 
 for i in range(0, 7):
-    input_angka = int(input(f"Masukkan angka ke-{i + 1} : "))
+    input_angka = int(input(f"Masukkan angka ke-{i +1} = "))
     angka.append(input_angka)
     total += input_angka
 
 rata_rata = total / len(angka)
 
-print("-"*16)
-print(f"Rata-rata : {rata_rata}")
-
 for i in range(len(angka)):
+    
     jarak = rata_rata - angka[i]
 
-    jarak = abs(jarak) 
+    jarak = abs(jarak)
 
     if i == 0 :
         jarak_terdekat = jarak
-        angka_terdekat = angka[0]
-    elif jarak_terdekat > jarak :
+        angka_terdekat = angka[i]
+    elif jarak < jarak_terdekat :
         jarak_terdekat = jarak
         angka_terdekat = angka[i]
 
-print(f"Angka terdekat = {angka_terdekat}")
+print(f"Rata-rata = {rata_rata}")
+
+print(f"Angka Terdekat dari rata-rata = {angka_terdekat}")
