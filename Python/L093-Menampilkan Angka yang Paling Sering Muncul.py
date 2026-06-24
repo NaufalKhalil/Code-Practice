27257ee11f28a750648722ff9bd82361a05b3d22
NaufalKhalil <naufalkhalil.third@gmail.com>
2026-06-24 20:23:52 +0700

    update: 2026-06-24 20:23

--- Python/L093-Menampilkan Angka yang Paling Sering Muncul.py
+++ Python/L093-Menampilkan Angka yang Paling Sering Muncul.py
@@ -1,15 +1,30 @@
 angka = []
-jumlah_modus = 0
+# deklarasi julah_kemunculan_sebelumnya yang belum ada karena masih putaran ke 0
+jumlah_kemunculan_sebelumnya = 0
 
+# masukkan input user ke dalam array angka
 for i in range(0, 8):
-    input_angka = int(input(f"Masukkan angka ke-{i +1} = "))
+    input_angka = int(input(f"Masukkan angka ke-{i + 1} = "))
     angka.append(input_angka)
+
+# perulangan untuk array
+for i in range(len(angka)):
     
+    # deklarasi cari adalah angka array sekarang
+    cari = angka[i]
 
-for j in range(len(angka)):    
+    # reset jumlah kemunculan apabila perulangan "j" sudah selesai
+    jumlah_kemunculan = 0    
 
-    for i in range(len(angka)):
-        if angka[j] == angka[i]:
-            jumlah_modus += 1
- 
-            
+    # perulangan untuk jumlah angka yang di cari
+    for j in range(len(angka)):
+        if cari == angka[j] :
+            jumlah_kemunculan += 1
+    
+    if jumlah_kemunculan > jumlah_kemunculan_sebelumnya :
+        jumlah_kemunculan_sebelumnya = jumlah_kemunculan
+        angka_modus = angka[i]
+    
+print("-"*16)
+print(f"Angka paling sering muncul = {angka_modus}")
+print(f"Jumlah kemunculan = {jumlah_kemunculan_sebelumnya}")
