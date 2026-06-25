57364bd6593a3db22f1442b7afa8922f5496315d
NaufalKhalil <naufalkhalil.third@gmail.com>
2026-06-25 14:41:19 +0700

    update: 2026-06-25 14:41

--- Python/L100-Menampilkan Angka dengan Frekuensi dari Terbesar ke Terkecil.py
+++ Python/L100-Menampilkan Angka dengan Frekuensi dari Terbesar ke Terkecil.py
@@ -3,9 +3,6 @@ angka = []
 angka_sortir = []
 jumlah_muncul = []
 
-angka_sortir_terbanyak = []
-jumlah_muncul_terbanyak = []
-
 for i in range(0, 8):
     input_angka = int(input(f"Masukkan angka ke-{i + 1} = "))
     angka.append(input_angka)
@@ -22,6 +19,15 @@ for i in range(len(angka)):
         angka_sortir.append(angka[i])
         jumlah_muncul.append(total_muncul)
 
+for i in range(len(jumlah_muncul)):
+
+    for j in range(len(jumlah_muncul) - 1 ):
+
+        if jumlah_muncul[j] < jumlah_muncul[j + 1]:
+            jumlah_muncul[j], jumlah_muncul[j + 1] = jumlah_muncul[j + 1], jumlah_muncul[j]
+            angka_sortir[j], angka_sortir[j + 1] = angka_sortir[j + 1], angka_sortir[j]
+        
 print("-"*16)
 
-
+for i in range(len(angka_sortir)):
+    print(f"{angka_sortir[i]} muncul {jumlah_muncul[i]} kali")
