148451d7ef3fac653d5552111a44ac2c34bd3e66
NaufalKhalil <naufalkhalil.third@gmail.com>
2026-07-12 16:30:39 +0700

    update: 2026-07-12 16:30

---
+++ Python/L159-Analisis Password Sederhana.py
@@ -0,0 +1,23 @@
+def d_tampilkan_header():
+    print("-" * 24)
+    print("Selamat Datang di Website NauShop")
+    print("-" * 24)
+
+def d_pembuatan_akun():
+    user_name = input("Masukkan Username : ")
+    password = input("Masukkan Password : ")
+    return user_name, password 
+
+def d_memisahkan_karakter_huruf(password):
+    password
+
+
+def kekuatan_password():
+    None
+
+while True :
+    d_tampilkan_header()
+
+
+
+
